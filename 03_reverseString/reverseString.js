9c99f4d9cb3f403dc8b5d2ef7203546a30a8c587
Andrew Heckman <93686608+AndrewHeckman@users.noreply.github.com>
2024-10-27 16:38:07 -0500

    Complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let reversedString = "";
+  for (let i = -1; i >= string.length * -1; i--) {
+    reversedString += string.at(i);
+  }
+  return reversedString;
 };
+
+// console.log(reverseString("hello"));
 
 // Do not edit below this line
 module.exports = reverseString;
