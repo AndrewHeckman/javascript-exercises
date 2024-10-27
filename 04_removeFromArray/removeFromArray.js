f6bed80871d58d5c086349911faa50341165f520
Andrew Heckman <93686608+AndrewHeckman@users.noreply.github.com>
2024-10-27 16:59:09 -0500

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...badNums) {
+  const newArray = [];
+  for (let num of array) {
+    if (badNums.includes(num)) continue;
+    newArray.push(num);
+  }
+  return newArray;
 };
 
 // Do not edit below this line
