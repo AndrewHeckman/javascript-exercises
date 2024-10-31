c0d27f62cf0114e19348e4170364f103aad2e5c0
Andrew Heckman <93686608+AndrewHeckman@users.noreply.github.com>
2024-10-30 23:43:19 -0500

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  let oldest;
+  let maxAge = 0;
+  const date = new Date().getFullYear();
+  for (const person of people) {
+    if (person.yearOfDeath === undefined) {
+      if (date - person.yearOfBirth > maxAge) {
+        maxAge = date - person.yearOfBirth;
+        oldest = person;
+      }
+    }
+    if (person.yearOfDeath - person.yearOfBirth > maxAge) {
+      maxAge = person.yearOfDeath - person.yearOfBirth;
+      oldest = person;
+    }
+  }
+  return oldest
 };
 
 // Do not edit below this line
