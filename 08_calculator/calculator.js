fa128213e01b206e9b4963f086654dd1203d99e6
Andrew Heckman <93686608+AndrewHeckman@users.noreply.github.com>
2024-10-30 22:41:36 -0500

    Complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,36 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	return nums.reduce(add, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.reduce(function (x, y) {
+    return x * y;
+  });
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num <= 1) return 1;
+  
+  let sum = num;
+	while (num > 1) {
+    sum *= --num;
+  }
+
+  return sum;
 };
+
 
 // Do not edit below this line
 module.exports = {
