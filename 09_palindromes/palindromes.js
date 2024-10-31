f3e16c2b9da38bc198f459ae0588e61add93b686
Andrew Heckman <93686608+AndrewHeckman@users.noreply.github.com>
2024-10-30 23:07:34 -0500

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const arr = string.toLowerCase().split("").filter(function (char) {
+    return "abcdefghijklmnopqrstuvwxyz0123456789".includes(char);
+  });
+  let revArr = arr.toReversed();
+  return arr.join("") === revArr.join("");
 };
 
 // Do not edit below this line
